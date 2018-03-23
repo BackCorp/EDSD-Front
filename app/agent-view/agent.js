cec76de0cf14eda49810637fe6ae0894b4894572
Brice Djilo <dbrice@uno.edu>
2018-03-23 03:39:39 -0600

    Added MyEdsdCtrl

--- app/agent-view/agent.js
+++ app/agent-view/agent.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('App.agent', ['ngRoute', 'ngCookies', 'ngSanitize'])
+angular.module('App.agent', ['ngRoute', 'ngCookies', 'ngSanitize', 'smart-table'])
 
 .config(['$routeProvider', function($routeProvider, $location, storageService) {
     $routeProvider.when('/agent', {
@@ -204,8 +204,12 @@ angular.module('App.agent', ['ngRoute', 'ngCookies', 'ngSanitize'])
         console.log($scope.primesIndices);
         $http({
             method: 'POST',
-            url: 'http://localhost:8080/api/edsd/primes/test/',
-            data: { primesGrade: $scope.primesGrade, primesIndices: $scope.primesIndices }
+            url: 'http://localhost:8080/api/edsd/primes/',
+            data: {
+                primesGrade: $scope.primesGrade,
+                primesIndices: $scope.primesIndices,
+                requesterAccountNumber: $scope.requester.accountNumber
+            }
         }).then(
             function(resp) {
                 console.log(resp);
@@ -219,6 +223,49 @@ angular.module('App.agent', ['ngRoute', 'ngCookies', 'ngSanitize'])
 
     };
 
+}])
+
+.controller('MyEdsdCtrl', ['$scope', function($scope) {
+    var firstnames = ['Laurent', 'Blandine', 'Olivier', 'Max'];
+    var lastnames = ['Renard', 'Faivre', 'Frere', 'Eponge'];
+    var dates = ['1987-05-21', '1987-04-25', '1955-08-27', '1966-06-06'];
+    var id = 1;
+
+    function generateRandomItem(id) {
+
+        var firstname = firstnames[Math.floor(Math.random() * 3)];
+        var lastname = lastnames[Math.floor(Math.random() * 3)];
+        var birthdate = dates[Math.floor(Math.random() * 3)];
+        var balance = Math.floor(Math.random() * 2000);
+
+        return {
+            id: id,
+            firstName: firstname,
+            lastName: lastname,
+            birthDate: new Date(birthdate),
+            balance: balance
+        }
+    }
+
+    $scope.rowCollection = [];
+
+    for (id; id < 5; id++) {
+        $scope.rowCollection.push(generateRandomItem(id));
+    }
+
+    //add to the real data holder
+    $scope.addRandomItem = function addRandomItem() {
+        $scope.rowCollection.push(generateRandomItem(id));
+        id++;
+    };
+
+    //remove to the real data holder
+    $scope.removeItem = function removeItem(row) {
+        var index = $scope.rowCollection.indexOf(row);
+        if (index !== -1) {
+            $scope.rowCollection.splice(index, 1);
+        }
+    }
 }])
 
 .directive('typeaheadFindRequester', function() {
